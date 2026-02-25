fa17e66877ff5d4e0950025ae9af1bbd1e76f4a6
mostafa-mahmod71 <mostafamahmod5550@gmail.com>
2026-02-25 16:16:13 +0200

    add feature: logout

--- src/app/layouts/components/navbar/navbar.component.ts
+++ src/app/layouts/components/navbar/navbar.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { initFlowbite } from 'flowbite';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { Userinfo } from '../../../userinfo.interface';
 @Component({
   selector: 'app-navbar',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent implements OnInit {
-  ngOnInit(): void {
-    initFlowbite();
+export class NavbarComponent {
+  private readonly router = inject(Router);
+  showdrop: boolean = false;
+  showlogoutmodul: boolean = false;
+  confirmlogout: boolean = false;
+  userinfo: Userinfo = JSON.parse(localStorage.getItem('socialUser') || '{}');
+  logout(): void {
+    localStorage.setItem('socialUser', '');
+    localStorage.setItem('socialToken', '');
+    this.router.navigate(['/login']);
+  }
+  logoutmodul(): void {
+    this.showlogoutmodul = !this.showlogoutmodul;
+    this.showdrop = false;
   }
 }
